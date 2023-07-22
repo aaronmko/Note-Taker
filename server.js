8b53c48c7979ecf987859ecbe3e494690511e154
minkiko@gmail.com <minkiko@gmail.com>
2023-07-22 11:42:11 -0500

    added middleware

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const allNotes = require('./db/db.json');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.get('/api/notes', (req, res) => {
